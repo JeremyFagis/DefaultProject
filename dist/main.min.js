15ef854f8d52c4ed8926eb298046846b0502f0a7
Jeremy Fagis <jeremy@fagis.fr>
2014-06-04 14:32:36 +0200

    DIsabled simple input file in IE.

--- dist/main.min.js
+++ dist/main.min.js
@@ -17,7 +17,8 @@ $(document).ready(function(){
             wrapperClass: "input-file-wrapper",
             defaultText: 'Envoyer un ficher',
             iconHtml: '<i class="icon-upload-cloud" />'
-        };
+        },
+        isIE = !!window.ActiveXObject;
 
     function Plugin ( element, options ) {
         this.element = element;
@@ -29,18 +30,20 @@ $(document).ready(function(){
 
     Plugin.prototype = {
         init: function () {
-            var element = $(this.element);
-            var defaultText = element.data('default-text') || this.settings.defaultText;
-            var value = element.val() || defaultText;
+            if (!isIE) {
+                var element = $(this.element);
+                var defaultText = element.data('default-text') || this.settings.defaultText;
+                var value = element.val() || defaultText;
 
-            element.wrap('<div class="' + this.settings.wrapperClass + '"/>');
-            $('<span class="filename"/>').text(value).prependTo(element.parent());
-            $(this.settings.iconHtml).prependTo(element.parent());
+                element.wrap('<div class="' + this.settings.wrapperClass + '"/>');
+                $('<span class="filename"/>').text(value).prependTo(element.parent());
+                $(this.settings.iconHtml).prependTo(element.parent());
 
-            element.on('change', function(){
-                var fileName = $(this).val().split('\\').pop();
-                $(this).siblings('span').html(fileName);
-            });
+                element.on('change', function(){
+                    var fileName = $(this).val().split('\\').pop();
+                    $(this).siblings('span').html(fileName);
+                });
+            }
         }
     };
 
