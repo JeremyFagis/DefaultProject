61d576ffe7667e688934501a3c71403b87bce2ca
Jeremy Fagis <jeremy@fagis.fr>
2014-06-05 14:25:05 +0200

    Added tabs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
     $(".selector").simpleSelector();
+
     $('input[type="file"]').simpleInputFile();
+
+    $('.nav-tabs li:not(.disabled) a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
 });
