e1f7d389fd56a87d5966775f731bd3c53424c0f5
Jeremy Fagis <jeremy@fagis.fr>
2014-12-29 16:51:37 +0100

    Fixed #10.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -17,4 +17,14 @@ $(document).ready(function(){
             $(this).children('a').append('<span class="submenu-icon"></span>');
         }
     });
+
+    if($('.page-title').length > 0) {
+        $('.page-title').addClass('hover');
+    }
+});
+
+$(window).load(function(){
+    setTimeout(function() {
+        $('.page-title').removeClass('hover');
+    }, 1500);
 });
